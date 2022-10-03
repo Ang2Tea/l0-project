92f3f4468cfbe92df6d9ba398791f8b59e9d8295
ang2tea <zazasu12@gmail.com>
2022-10-03 12:57:37 +0300

    Rework work with routes

--- Service/routes/route.go
+++ Service/routes/route.go
@@ -4,18 +4,23 @@ import (
 	"encoding/json"
 	"net/http"
 
-	"github.com/Ang2Tea/L0Project/caches"
 	"github.com/Ang2Tea/L0Project/models"
+	"github.com/Ang2Tea/L0Project/util"
 	"github.com/gorilla/mux"
+	"github.com/rs/cors"
+	"github.com/sirupsen/logrus"
 )
 
 type Route struct {
-	Cache  caches.Cache
-	Config models.Config
+	Cache *util.Cache
 }
 
-func (route *Route) Start() error {
-	return http.ListenAndServe(route.Config.Server.Port, route.createRoutes())
+func (route *Route) Start(config models.HttpServerConfig) error {
+	c := cors.New(cors.Options{
+		AllowedOrigins: []string{"*"},
+		AllowedMethods: []string{"GET"},
+	})
+	return http.ListenAndServe(":"+config.Port, c.Handler(route.createRoutes()))
 }
 
 func (route *Route) createRoutes() *mux.Router {
@@ -27,18 +32,21 @@ func (route *Route) createRoutes() *mux.Router {
 	return r
 }
 
-func getOrdersRoute(cache caches.Cache) func(w http.ResponseWriter, r *http.Request) {
+func getOrdersRoute(cache *util.Cache) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		json.NewEncoder(w).Encode(cache.ToSlice())
+
+		logrus.Info("Get orders")
 	}
 }
 
-func getOrderRoute(cache caches.Cache) func(w http.ResponseWriter, r *http.Request) {
+func getOrderRoute(cache *util.Cache) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		params := mux.Vars(r)
 
+		logrus.Info("Get order by order_uid")
 		if order, check := cache.Get(params["uid"]); check {
 			json.NewEncoder(w).Encode(order)
 			return
